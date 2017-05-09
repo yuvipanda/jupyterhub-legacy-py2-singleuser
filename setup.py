8d05f992c0474becdec71b857d7f397da7b6707b
yuvipanda <yuvipanda@gmail.com>
2017-05-09 01:10:28 -0700

    Fail to install on Python3

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+from __future__ import print_function
 from setuptools import setup, find_packages
+import sys
+
+v = sys.version_info
+if v[:2] != (2, 7):
+    print("jupyterhub-legacy-singleuser package is only to be used on Python2.7.\n" + \
+          "Use jupyterhub package on Python3.3+", file=sys.stderr)
+    sys.exit(1)
 
 setup(
-    name='jupyterhub-singleuser',
+    name='jupyterhub-legacy-singleuser',
     version='0.7.2',
-    description='Single-user server for running JupyterNotebooks with JupyterHub',
-    url='https://github.com/yuvipanda/jupyterhub-singleuser',
+    description='Single-user server for running Jupyter Notebooks (on Python2) with JupyterHub',
+    url='https://github.com/yuvipanda/jupyterhub-legacy-singleuser',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
     license='3 Clause BSD',
